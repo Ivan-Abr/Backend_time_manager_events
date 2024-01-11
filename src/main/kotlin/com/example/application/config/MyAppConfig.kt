bc43986140feff586ab90f433be1df88fa2f0c80
alexey <mister.alex49@yandex.ru>
2024-01-12 00:35:55 +0300

    fixes

--- src/main/kotlin/com/example/application/config/MyAppConfig.kt
+++ src/main/kotlin/com/example/application/config/MyAppConfig.kt
@@ -2,16 +2,28 @@ package com.example.application.config
 
 import com.example.application.filter.HeadersLoggingFilter
 import com.example.application.repository.UserRepo
+import io.swagger.v3.oas.annotations.ExternalDocumentation
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.annotations.servers.Server
+import io.swagger.v3.oas.models.Components
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+@OpenAPIDefinition(
+        servers = [
+            Server(url = "http://85.175.194.251:50443/api/event-service/")
+        ],
+        externalDocs = ExternalDocumentation(url = "http://85.175.194.251:50443/api/event-service/timemanager-openapi", description = "External documentation"),
+)
 @Configuration
 class MyAppConfig {
 
     @Autowired
     private lateinit var userRepo: UserRepo
+
     @Autowired
     private lateinit var envConfig: EnvConfig
 
@@ -20,7 +32,7 @@ class MyAppConfig {
         val registrationBean: FilterRegistrationBean<HeadersLoggingFilter> =
                 FilterRegistrationBean<HeadersLoggingFilter>()
         registrationBean.setFilter(
-                HeadersLoggingFilter(userRepo,envConfig)
+                HeadersLoggingFilter(userRepo, envConfig)
         )
         registrationBean.order = -1
         registrationBean.setName("fooBar")
